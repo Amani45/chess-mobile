eeec32972614a6c9feed3252c36eba84548898d7
Amani <am.m4@hotmail.com>
2020-02-08 16:19:59 +0300

    adding select chessmen

--- src/app/home/chess/chessmen-select/chessmen-select.component.ts
+++ src/app/home/chess/chessmen-select/chessmen-select.component.ts
@@ -25,21 +25,30 @@ export class ChessmenSelectComponent implements OnInit {
   }
 
   onAction(){
-    console.log("Knight :", this.knight)
-    for (let index = 0; index < this.knight; index++) {
-      this.chessmen.push('knight')
-    }
-    for (let index = 0; index < this.queen; index++) {
-      this.chessmen.push('queen')
-    }
-    for (let index = 0; index < this.bishop; index++) {
-      this.chessmen.push('bishop')
-    }
-    for (let index = 0; index < this.rook; index++) {
-      this.chessmen.push('rook')
+    console.log(this.knight, this.bishop, this.queen, this.rook, )
+
+    if(this.knight == 0 && this.bishop == 0 && this.queen == 0 && this.rook == 0){
+      alert("Please select at least one chessman")
+      return
+    }else {
+      console.log("Knight :", this.knight)
+      for (let index = 0; index < this.knight; index++) {
+        this.chessmen.push('knight')
+      }
+      for (let index = 0; index < this.queen; index++) {
+        this.chessmen.push('queen')
+      }
+      for (let index = 0; index < this.bishop; index++) {
+        this.chessmen.push('bishop')
+      }
+      for (let index = 0; index < this.rook; index++) {
+        this.chessmen.push('rook')
+      }
+  
+      this.chessmenSelected.emit(this.chessmen)
     }
 
-    this.chessmenSelected.emit(this.chessmen)
+
 
   }
 
